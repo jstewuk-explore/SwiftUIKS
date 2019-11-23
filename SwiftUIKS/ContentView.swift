b9328973afd99d2a72194d0d27134fff75f11a46
Jim Stewart <jim@stewartstuff.com>
2019-11-22 21:20:33 -0600

    Floating button Yay!

--- SwiftUIKS/ContentView.swift
+++ SwiftUIKS/ContentView.swift
@@ -8,10 +8,72 @@
 
 import SwiftUI
 
+class ShowingSheet: ObservableObject {
+    @Published var value = false
+}
+
 struct ContentView: View {
+    @ObservedObject var newShowingSheet = ShowingSheet()
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack {
+                Text("Hello, World!")
+                Button(action: { self.newShowingSheet.value.toggle() }) {
+                    Text("Show Sheet")
+                }
+                Text("showingSheet: \(self.newShowingSheet.value ? "true": "false")")
+            }.sheet(isPresented: $newShowingSheet.value) {
+                DaSheet(showingSheet: self.newShowingSheet)
+            }
+            FloatingButton(showingSheet: self.newShowingSheet)
+        }
     }
+}
+
+struct FloatingButton: View {
+    @ObservedObject var showingSheet: ShowingSheet
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.showingSheet.value.toggle()
+                }, label: {
+                    Text("+")
+                        .font(.system(.largeTitle))
+                        .frame(width: 77, height: 70)
+                        .foregroundColor(.white)
+                        .padding(.bottom, 7)
+                        .background(Color.blue)
+                        .cornerRadius(38.5)
+                        .padding()
+                        .shadow(color: Color.black.opacity(0.3),
+                                radius: 3,
+                                x: 3,
+                                y: 3)
+                })
+            }
+        }
+    }
+}
+
+struct DaSheet: View {
+    @ObservedObject var showingSheet: ShowingSheet
+    
+    var body: some View {
+        VStack {
+            Text("Da sheet -- showingSheet: \(self.showingSheet.value ? "true" : "false")")
+            Button(action: { self.showingSheet.value.toggle() }) {
+                Text("Hide Sheet")
+            }
+        }
+    }
+}
+
+struct Item {
+    let uuid = UUID()
+    let value: String
 }
 
 struct ContentView_Previews: PreviewProvider {
